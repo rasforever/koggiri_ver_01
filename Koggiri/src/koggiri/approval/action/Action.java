660db04fd39876aafaa496808517d33617b5f99e
유승현 <rasforever17@gmail.com>
2017-04-26 16:47:57 +0900

    유승현 결재 작업중 1

---
+++ Koggiri/src/koggiri/approval/action/Action.java
@@ -0,0 +1,9 @@
+package koggiri.approval.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public interface Action {
+	public ActionFoward execute(HttpServletRequest request, HttpServletResponse response) throws Exception;
+}
