3887408933d2219989e447bbee136a26a4ae8dbb
8yunghwi\Byunghwi <gpdldy91@gmail.com>
2017-05-07 17:03:19 +0900

    파일 업로드 -> 디비연동까지

--- Koggiri/src/koggiri/document/action/Doc_Action.java
+++ Koggiri/src/koggiri/document/action/Doc_Action.java
@@ -1,10 +1,12 @@
 package koggiri.document.action;
 
+import java.io.IOException;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public interface Doc_Action {
 	
-	public Doc_ActionForward execute(HttpServletRequest request, HttpServletResponse response);
+	public Doc_ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws IOException;
 
 }
--- Koggiri/src/koggiri/document/action/Doc_InsertAction.java
+++ Koggiri/src/koggiri/document/action/Doc_InsertAction.java
@@ -1,9 +1,14 @@
 package koggiri.document.action;
 
+import java.io.IOException;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.httpclient.methods.multipart.MultipartRequestEntity;
+
+import com.oreilly.servlet.MultipartRequest;
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
 
 import koggiri.document.model.Doc_Board;
 import koggiri.document.model.Doc_BoardDao;
@@ -11,7 +16,7 @@ import koggiri.document.model.Doc_BoardDao;
 public class Doc_InsertAction implements Doc_Action {
 
 	@Override
-	public Doc_ActionForward execute(HttpServletRequest request, HttpServletResponse response) {
+	public Doc_ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws IOException {
 	
 		Doc_BoardDao doc_dao = Doc_BoardDao.getInstance();
 		Doc_Board doc_board = new Doc_Board();
@@ -20,15 +25,28 @@ public class Doc_InsertAction implements Doc_Action {
 		String uploadPath = request.getRealPath("doc_upload"); // 1. 겟리얼패스로 만든 doc_upload 폴더까지의 절대전체경로 구함
 		int size = 20 * 1024 * 1024;// 2. 크기를 20mb로 정한다
 		
+		System.out.println("test" + uploadPath);
+		MultipartRequest multi = new MultipartRequest(request, uploadPath, size, "utf-8", new DefaultFileRenamePolicy());
+		// 3. "utf-8"로 인코딩해준다. 
+		// 4. new DefaultFileRenamePolicy() -> 중첩정책 자동으로 해줌
+		//multi로 모든 값들을 받아야한다.
+		//이  생성했을때가 파일이 업로드 되는 시점.
 	
 		
-		doc_board.setF_title(request.getParameter("f_title"));
-		doc_board.setF_content(request.getParameter("f_content"));
-		doc_board.setF_name(request.getParameter("f_name"));
-		doc_board.setF_pwd(request.getParameter("f_pwd"));
-		doc_board.setF_fname(request.getParameter("f_fname"));
+		doc_board.setF_title(multi.getParameter("f_title"));
+		doc_board.setF_content(multi.getParameter("f_content"));
+		doc_board.setF_name(multi.getParameter("f_name"));
+		doc_board.setF_pwd(multi.getParameter("f_pwd"));
+		doc_board.setF_fname(multi.getParameter("f_fname"));
 		
-		System.out.println(request.getParameter("f_title"));
+		//파일업로드인지 아닌지 확인하는 부분
+		if(multi.getFilesystemName("f_fname") != null){ //getFilesystemName를 사용하면 업로드 되었는지 안되었는지 확인 가능.
+			String f_fname = multi.getFilesystemName("f_fname"); // 업로드한 파일이름을 뽑아서 DB에 저장
+			doc_board.setF_fname(f_fname); //doc_board 객체에 넣음으로써 DB에 전달됨.
+		}
+		else{
+			doc_board.setF_fname("");
+		}
 		
 		System.out.println(doc_board.toString());
 		doc_dao.doc_insertBoard(doc_board);
